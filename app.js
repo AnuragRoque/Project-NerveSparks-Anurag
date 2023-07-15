2c44373692e8276dbc47844118b066e75c690b94
Anurag Singh <anirock2050@gmail.com>
2023-07-16 04:04:22 +0530

    updated connection

--- app.js
+++ app.js
@@ -11,6 +11,33 @@ app.use('/auth', authRoutes);
 app.use('/users', userRoutes);
 app.use('/dealerships', dealershipRoutes);
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://anuragsingh2445:yiGTroxdfeV4WnSF@cluster0.y58xddm.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
 });
+
+async function startServer() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    // Start the server after the database connection is established
+    app.listen(3000, () => {
+      console.log('Server is running on port 3000');
+    });
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+startServer();
--- config.js
+++ config.js
@@ -1,5 +1,9 @@
 module.exports = {
     port: process.env.PORT || 3000,
-    jwtSecret: process.env.JWT_SECRET || 'secret',
+    jwtSecret: process.env.JWT_SECRET || '9688d6471242075a4cc3e9e68bb5bfa91409daef490178fa51d278b2595d481046efef687e889201fbc7437a25a2b214bf95548cc90a5f76841b1cdd7bf32c6e',
     dbUrl: process.env.DB_URL || 'mongodb+srv://anuragsingh2445:yiGTroxdfeV4WnSF@cluster0.y58xddm.mongodb.net/'
   };
+
+
+//MONGODB_URI=mongodb+srv://anuragsingh2445:yiGTroxdfeV4WnSF@cluster0.y58xddm.mongodb.net/
+//JWT_SECRET=9688d6471242075a4cc3e9e68bb5bfa91409daef490178fa51d278b2595d481046efef687e889201fbc7437a25a2b214bf95548cc90a5f76841b1cdd7bf32c6e
