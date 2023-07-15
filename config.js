dad438a6aa4a04354b102af706ed0ca5c3f26899
Anurag Singh <anirock2050@gmail.com>
2023-07-16 03:26:29 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
     port: process.env.PORT || 3000,
     jwtSecret: process.env.JWT_SECRET || 'secret',
-    dbUrl: process.env.DB_URL || 'mongodb://localhost:27017/myapp'
+    dbUrl: process.env.DB_URL || 'mongodb+srv://anuragsingh2445:yiGTroxdfeV4WnSF@cluster0.y58xddm.mongodb.net/'
   };
